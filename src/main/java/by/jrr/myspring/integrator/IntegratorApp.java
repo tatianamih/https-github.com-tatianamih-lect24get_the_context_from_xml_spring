cc5c4702a40625ca7f4b5e1142492dfccceff41e
“tatianamih” <tmihalchenko66@gmail.com>
2020-11-05 01:56:37 +0300

    IntegratorApp

---
+++ src/main/java/by/jrr/myspring/integrator/IntegratorApp.java
@@ -0,0 +1,12 @@
+package by.jrr.myspring.integrator;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class IntegratorApp {
+    public static void main(String[] args) {
+       ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
+    }
+
+}

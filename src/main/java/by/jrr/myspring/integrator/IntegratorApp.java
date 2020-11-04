02ca1fa20e31b04bc61889e9f40632a47d7c01f5
“tatianamih” <tmihalchenko66@gmail.com>
2020-11-05 02:02:15 +0300

    app

--- src/main/java/by/jrr/myspring/integrator/IntegratorApp.java
+++ src/main/java/by/jrr/myspring/integrator/IntegratorApp.java
@@ -1,12 +1,10 @@
 package by.jrr.myspring.integrator;
 
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IntegratorApp {
     public static void main(String[] args) {
-       ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
     }
-
 }
